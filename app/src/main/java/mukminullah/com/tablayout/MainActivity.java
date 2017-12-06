eb5aff790a0c41637e8a7a2229c033f986f8389f
NaturalizerINA <naturalizer.ina@gmail.com>
2017-12-06 13:37:11 +0700

    Adding Image Demo

--- app/src/main/java/mukminullah/com/tablayout/MainActivity.java
+++ app/src/main/java/mukminullah/com/tablayout/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
